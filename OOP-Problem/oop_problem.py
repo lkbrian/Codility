a59bbe5dacce386ced970fee7011b41bce3a6410
lkbrian <lkbrian742@gmail.com>
2024-03-17 20:58:12 +0300

    Updates

--- OOP-Problem/oop_problem.py
+++ OOP-Problem/oop_problem.py
@@ -12,7 +12,11 @@ def solution(data):
 
 scoreboard = [
     {"name": "Habanero Hillary", "chickenwings": 5, "hamburgers": 17, "hotdogs": 11},
-    {"name": "Big Bob", "chickenwings": 20, "hamburgers": 4, "hotdogs": 11},
+    {"name": "Allen shams", "chickenwings": 12, "hamburgers": 6, "hotdogs": 22},
+    {"name": "Anne Shiko", "chickenwings": 9, "hamburgers": 20, "hotdogs": 1},
+    {"name": "Brian Kip", "chickenwings": 8, "hamburgers": 14, "hotdogs": 5},
+    {"name": "Mariam Senzi", "chickenwings": 23, "hamburgers": 7, "hotdogs": 8},
+    {"name": "Big Bob", "chickenwings": 20, "hamburgers": 4, "hotdogs": 3},
 ]
 
 solution(scoreboard)
