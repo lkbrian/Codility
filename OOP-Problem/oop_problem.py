0dd3cde063d1d26bf3a3f7f8313b9a376ed8f8d0
lkbrian <lkbrian742@gmail.com>
2024-03-19 14:07:49 +0300

    Completed Assignments

--- OOP-Problem/oop_problem.py
+++ OOP-Problem/oop_problem.py
@@ -1,5 +1,5 @@
-def solution(data):
-    items = sorted(data, key=lambda data: data["name"])
+def solution(items):
+    data = []
     for item in items:
         result = (
             (item["hamburgers"] * 3)
@@ -7,15 +7,20 @@ def solution(data):
             + (item["hotdogs"] * 2)
         )
         name = item["name"]
-        print(f"name: {name}, score: {result}")
-
+        output ={"name": name, "score": result}
+        data.append(output)
+    scoreboard_result = sorted(data,key=lambda data: data["score"],reverse=True)
+    # print(scoreboard_result)
+    #print the results line by line
+    for score in scoreboard_result:
+        print(score)
 
 scoreboard = [
     {"name": "Habanero Hillary", "chickenwings": 5, "hamburgers": 17, "hotdogs": 11},
     {"name": "Allen shams", "chickenwings": 12, "hamburgers": 6, "hotdogs": 22},
     {"name": "Anne Shiko", "chickenwings": 9, "hamburgers": 20, "hotdogs": 1},
     {"name": "Brian Kip", "chickenwings": 8, "hamburgers": 14, "hotdogs": 5},
-    {"name": "Mariam Senzi", "chickenwings": 23, "hamburgers": 7, "hotdogs": 8},
+    {"name": "Mariam Senzia", "chickenwings": 3, "hamburgers": 7, "hotdogs": 8},
     {"name": "Big Bob", "chickenwings": 20, "hamburgers": 4, "hotdogs": 3},
 ]
 
